c5b5b5eccae2f0eeaa6c8a9cff5af24a259867ed
ChangHoSe <sas1397@naver.com>
2024-08-30 15:25:21 +0900

    Feature : 기초 파일 upload

---
+++ src/main/java/com/quiteSeoul/QuiteSeoulApplication.java
@@ -0,0 +1,13 @@
+package com.quiteSeoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuiteSeoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuiteSeoulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quiteSeoul/QuiteSeoulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiteSeoul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuiteSeoulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
