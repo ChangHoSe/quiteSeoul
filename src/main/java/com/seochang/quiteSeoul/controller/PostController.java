f0f2a4dce6d3f09cb3e401ecc8542cb1194528e4
ChangHoSe <sas1397@naver.com>
2024-10-22 20:49:22 +0900

    기초 페이지 작성

--- src/main/java/com/seochang/quiteSeoul/controller/PostController.java
+++ src/main/java/com/seochang/quiteSeoul/controller/PostController.java
@@ -1,4 +1,13 @@
 package com.seochang.quiteSeoul.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PostController {
+
+    @GetMapping("/login")
+    public String initial() {
+        return "login";
+    }
 }
